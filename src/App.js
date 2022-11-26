3a7bce463049235413874cc7451cbc4677094ff5
Raphael Quinones <wapquinones28@gmail.com>
2022-11-27 00:16:48 +0800

    connect wallet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
-import * as React from "react";
+import React, {useEffect, useState} from "react";
 import { ethers } from "ethers";
 import './App.css';
 
+const getEthereumObject = () => window.ethereum;
+
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    if (!ethereum) {
+      console.error("Make sure you have metamask!");
+      return null;
+    };
+
+    console.log("We have ethereum object: ", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if(accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
 
   const wave = () => {
     
   }
+
+  const connectWallet = async () => {
+    try{
+      const ethereum = getEthereumObject();
+      if(!ethereum){
+        alert("Get Metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method:"eth_requestAccounts",
+      });
+
+      console.log("Connected: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error){
+      console.error(error)
+    }
+  }
+
+  useEffect( () => {
+    const account = "";
+    async function findMeta(){
+      account = await findMetaMaskAccount();
+    }
+
+    findMeta();
+    
+    if(account !== null){
+      setCurrentAccount(account);
+    }
+  }, [])
+
   
   return (
     <div className="mainContainer">
@@ -23,6 +84,14 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {
+          !currentAccount && (
+            <button className = "waveButton" onClick = {connectWallet}>
+              Connect Wallet
+            </button>
+          )
+        }
       </div>
     </div>
   );
