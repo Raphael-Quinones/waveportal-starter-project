77d9ce338d637af348864986e7a8049b93e7ec38
Raphael Quinones <wapquinones28@gmail.com>
2022-11-28 02:37:06 +0800

    added gas limit

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ export default function App() {
         let count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
 
-        const waveTxn = await wavePortalContract.wave(message);
+        const waveTxn = await wavePortalContract.wave(message, { gasLimit: 300000 });
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
